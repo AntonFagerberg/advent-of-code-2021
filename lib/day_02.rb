d0659602c7a95585f8a9e4196d7a0695de24a8c6
Anton Fagerberg <anton@antonfagerberg.com>
2021-12-02 22:29:34 +0100

    Completed day 2

---
+++ lib/day_02.rb
@@ -0,0 +1,39 @@
+class Day02
+  def part1(input)
+    x = 0
+    y = 0
+    
+    input.map { |e| 
+      parts = e.split(' ') 
+      dir = parts[0]
+      value = parts[1].to_i
+      
+      x += value if dir == 'forward'
+      y += value if dir == 'down'
+      y -= value if dir == 'up'
+    }
+    
+    x * y
+  end
+  
+  def part2(input)
+    x = 0
+    y = 0
+    aim = 0
+    
+    input.map do |e| 
+      parts = e.split(' ') 
+      dir = parts[0]
+      value = parts[1].to_i
+      
+      aim += value if dir == 'down'
+      aim -= value if dir == 'up'
+      if dir == 'forward'
+        x += value 
+        y += aim * value
+      end
+    end
+    
+    x * y
+  end
+end
