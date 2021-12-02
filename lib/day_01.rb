883dab32bd7f41ce92889aa35b09923ec1205c08
Anton Fagerberg <anton@antonfagerberg.com>
2021-12-02 21:58:08 +0100

    Completed day 1

--- lib/day_01.rb
+++ lib/day_01.rb
@@ -1,11 +1,23 @@
 class Day01
-  def thing(input)
+  def part1(input)
     incs = 0
 
     for i in 1...input.size
       incs += 1 if input[i - 1].to_i < input[i].to_i
     end
 
+    incs
+  end
+  
+  def part2(input)
+    incs = 0
+    
+    for i in 3...input.size
+      sum1 = input[i - 3].to_i + input[i - 2].to_i + input [i - 1].to_i
+      sum2 = input[i - 2].to_i + input[i - 1].to_i + input[i].to_i
+      incs += 1 if sum1 < sum2
+    end
+    
     incs
   end
 end
--- test/day_01_test.rb
+++ test/day_01_test.rb
@@ -3,10 +3,18 @@ require_relative '../lib/day_01'
 
 class Day01Test < Minitest::Test
   def test_part1example
-    assert_equal Day01.new.thing(File.readlines('input/day01/example1')), 7
+    assert_equal 7, Day01.new.part1(File.readlines('input/day01/example1'))
   end
 
   def test_part1
-    assert_equal Day01.new.thing(File.readlines('input/day01/input')), 1226
+    assert_equal 1226, Day01.new.part1(File.readlines('input/day01/input'))
+  end
+  
+  def test_part2example
+    assert_equal 5, Day01.new.part2(File.readlines('input/day01/example1'))
+  end
+
+  def test_part2
+    assert_equal 1252, Day01.new.part2(File.readlines('input/day01/input'))
   end
 end
